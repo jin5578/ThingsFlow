7d0799322a2e90384a98b6a2fb89cc0df43b11d0
정현성 <jin55789@naver.com>
2020-10-28 17:08:24 +0900

    Image 넣어주는 Glide BindingAdapter로 추가

--- app/src/main/java/com/tistory/jeongs0222/thingsflow/ui/main/adapter/MainBindingAdapter.kt
+++ app/src/main/java/com/tistory/jeongs0222/thingsflow/ui/main/adapter/MainBindingAdapter.kt
@@ -1,9 +1,12 @@
 package com.tistory.jeongs0222.thingsflow.ui.main.adapter
 
 import android.annotation.SuppressLint
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.tistory.jeongs0222.thingsflow.model.OrgRepo
 import com.tistory.jeongs0222.thingsflow.ui.main.MainUiModel
 
@@ -21,4 +24,14 @@ fun issueList(recyclerView: RecyclerView, list: List<MainUiModel>?) {
 @BindingAdapter("introText")
 fun introText(textView: TextView, orgRepoValue: OrgRepo) {
     textView.text = orgRepoValue.org + " / " + orgRepoValue.repo
+}
+
+@BindingAdapter("issueImage")
+fun issueImage(imageView: ImageView, url: String) {
+    Glide.with(imageView)
+        .load(url)
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .skipMemoryCache(true)
+        .into(imageView)
+
 }
